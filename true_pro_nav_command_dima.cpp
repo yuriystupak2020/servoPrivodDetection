dfde779426df7e56e989a7985e8819121555cf2f
yuriystupak2020 <74398077+yuriystupak2020@users.noreply.github.com>
2023-09-11 10:42:06 +0300

    Add files via upload

---
+++ true_pro_nav_command_dima.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <vector>
+
+namespace math {
+    template <typename T>
+    struct real2 {
+        T x, y;
+        // Define squared_length() member function here--????
+        T squared_length() const {
+            return x * x + y * y;// ???Is it right count?
+        }
+        // Define subtraction operator for math::real2
+        real2 operator-(const real2& other) const {
+            return {x - other.x, y - other.y};
+        }
+    };
+}
+
+template < typename RealType >
+constexpr inline auto const true_proportional_navigation_command(
+    math::real2 < RealType > const& line_of_sight,
+    math::real2 < RealType > const& target_velocity,
+    math::real2 < RealType > const& interceptor_velocity,
+    RealType const& effective_navigation_ratio
+)
+{
+    auto const& line_of_sight_squared_length{ line_of_sight.squared_length()};
+    if (line_of_sight_squared_length < RealType{ 1e-12})
+        return math::real2 < RealType > { 0 };
+
+    auto const& Vtm{ target_velocity - interceptor_velocity };
+    return 
+        (
+            effective_navigation_ratio
+            *
+                ( line_of_sight | Vtm )
+            *   
+                ( line_of_sight ^ Vtm )
+            /
+                math::squared( line_of_sight_squared_length )
+        )
+    *
+        math::real2 < RealType > { -line_of_sight.y, line_of_sight.x};
+};
+
+int main() {
+    // Создаем объекты типа math::real2
+    math::real2<double> line_of_sight{1.0, 2.0};
+    math::real2<double> target_velocity{3.0, 4.0};
+    math::real2<double> interceptor_velocity{0.5, 0.5};
+    double effective_navigation_ratio = 0.1;
+
+    // Вызываем функцию true_proportional_navigation_command
+    auto result = true_proportional_navigation_command(line_of_sight, target_velocity, interceptor_velocity, effective_navigation_ratio);
+
+    // Выводим результат
+    std::cout << "Результат: (" << result.x << ", " << result.y << ")" << std::endl;
+
+    return 0;
+}
